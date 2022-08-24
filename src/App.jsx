84cc88cbd8a994a5e33f762e5f5faecc7258c12d
Larissa Pinheiro <rissapins@gmail.com>
2022-08-24 15:52:18 -0300

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { Header } from "./components/Header";
+
+import styles from "./styles/App.module.css";
+
+import "./styles/global.css";
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>posts</main>
+      </div>
+    </>
+  );
 }
