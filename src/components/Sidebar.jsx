4fe69680199714f0fce7c19a7e78ad8f235a6e61
Larissa Pinheiro <rissapins@gmail.com>
2022-08-24 21:39:39 -0300

    create sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilSimpleLine } from "phosphor-react"; // npm i phorphor-react > import no documento > ctrl+space dentro dos colchetes para visualizar os ícones
+
+import styles from "../styles/Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://cdn.discordapp.com/attachments/849417858991783936/1012157495269142578/azulll.png"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/larissa-pinheiro.png"
+        />
+
+        <strong>Larissa May</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20} weight="bold" />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
