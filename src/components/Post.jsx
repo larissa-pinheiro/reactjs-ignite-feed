0d43b115589532ccd19ab01d9be4cb6836a94598
Larissa Pinheiro <rissapins@gmail.com>
2022-08-24 21:38:29 -0300

    create post

---
+++ src/components/Post.jsx
@@ -0,0 +1,42 @@
+import styles from "../styles/Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header className={styles.header}>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/larissa-pinheiro.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Larissa May</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time
+          className={styles.time}
+          title="24 de Agosto às 20:50h"
+          dateTime="2022-08-24 20:50:02"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉<a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
