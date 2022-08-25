6bb46a70dc57d6d59d87afc85017ce0f55fb2581
Larissa Pinheiro <rissapins@gmail.com>
2022-08-25 16:54:05 -0300

    create comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,41 @@
+import { Trash } from "phosphor-react";
+import { ThumbsUp } from "phosphor-react";
+import styles from "../styles/Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/larissa-pinheiro.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Larissa May</strong>
+              <time
+                className={styles.time}
+                title="24 de Agosto às 20:50h"
+                dateTime="2022-08-24 20:50:02"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
