bcbb3967c4075f7a49f647cae7bba3d32ca93537
Larissa Pinheiro <rissapins@gmail.com>
2022-08-29 16:40:44 -0300

    add key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,6 +7,7 @@ import { Comment } from "./Comment";
 
 import styles from "../styles/Post.module.css";
 
+// author e content são propriedades
 export function Post({ author, content }) {
   // estado = variáveis que o componente precisa monitorar; em caso de alteração, ele irá avisar ao React para mostrar as novas informações em tela
 
@@ -70,10 +71,10 @@ export function Post({ author, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -99,7 +100,7 @@ export function Post({ author, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
